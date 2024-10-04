14f5ebd87e638cf0855ffb9429f31441a3a4b7ca
sc@t0a14 <sc@t0a14>
2024-10-04 21:44:01 +0900

    feat: 야구게임 시작 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,8 +19,11 @@ public class Application {
                 break;
             }
         }
-
-
         System.out.println(computerNumbers);
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+
+
     }
 }
