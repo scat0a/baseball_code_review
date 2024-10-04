9f9d5d9153e478eb80833a3fe3818885f1e6551c
sc@t0a14 <sc@t0a14>
2024-10-04 21:47:14 +0900

    feat: 사용자 입력 숫자 만들기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,21 +9,29 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> computerNumbers = new ArrayList<>();
-        while (true){
+        while (true) {
             for (int i = 0; i < 3; i++) {
-                int random = Randoms.pickNumberInRange(1,9);
-                if(!computerNumbers.contains(random)){
+                int random = Randoms.pickNumberInRange(1, 9);
+                if (!computerNumbers.contains(random)) {
                     computerNumbers.add(random);
                 }
             }
-            if(computerNumbers.size() == 3){
+            if (computerNumbers.size() == 3) {
                 break;
             }
         }
-        System.out.println(computerNumbers);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
+
+        String input = Console.readLine();
+        String[] userInputs = input.split("");
+
+        List<Integer> answers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            answers.add(Integer.parseInt(userInputs[i]));
+        }
+
 
 
     }
