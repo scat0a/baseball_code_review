06bcb024edd41867ed89843dc240b6bdebe99ae5
sc@t0a14 <sc@t0a14>
2024-10-04 21:41:28 +0900

    feat: 컴퓨터 숫자 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (true){
+            for (int i = 0; i < 3; i++) {
+                int random = Randoms.pickNumberInRange(1,9);
+                if(!computerNumbers.contains(random)){
+                    computerNumbers.add(random);
+                }
+            }
+            if(computerNumbers.size() == 3){
+                break;
+            }
+        }
+
+
+        System.out.println(computerNumbers);
     }
 }
